9c899b99c40f612eeebe68a8c222849a56f0f4cd
Vaughn Cedric L. Araneta <vaughncedriclipardoaraneta@gmail.com>
2025-07-05 01:46:41 +0800

    Add project files.

---
+++ qlogicae_connect_4/qlogicae_connect_4.cpp
@@ -0,0 +1,11 @@
+// qlogicae_connect_4.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnqlogicaeconnect4()
+{
+	QLogicaeCore::CliIO::instance().print("Hello World!");
+}
---
+++ qlogicae_connect_4/qlogicae_connect_4.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+void fnqlogicaeconnect4();
